f957f9c5d7d91b2cd4c44cff42b7bf0d21807cad
ximuli <ximuli666@163.com>
2021-04-25 21:39:51 +0800

    容器水平、垂直居中、尺寸计算、大屏适配

--- src/app.tsx
+++ src/app.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import {Switch, HashRouter} from 'react-router-dom';
 import {Route} from 'react-router-dom';
 import {Home} from './pages/home';
-import {Config} from './pages/config';
 
 function App() {
 
@@ -12,9 +11,6 @@ function App() {
         <Switch>
           <Route exact path="/">
             <Home/>
-          </Route>
-          <Route exact path="/config">
-            <Config/>
           </Route>
         </Switch>
       </HashRouter>
