8a3326c041a4db654e99091372ac5026e0e7d494
ximuli <ximuli666@163.com>
2021-05-13 20:37:44 +0800

    add footer

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,6 +4,7 @@ import { Chart1 } from '../components/chart-1';
 import { Chart2 } from '../components/chart-2';
 
 export const Home = () => {
+  const currentYear = new Date().getFullYear()
   return (
     <div className="home-header-wrapper">
       <header></header>
@@ -17,6 +18,7 @@ export const Home = () => {
         <section className="bordered section4"></section>
         <section className="bordered section5"></section>
       </main>
+      <footer> &copy; ximuli 2020-{ currentYear }</footer>
     </div>
   );
 };
