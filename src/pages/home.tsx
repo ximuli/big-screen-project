c50fb04ed1d838e718671fcbf08c84dc07facaad
ximuli <ximuli666@163.com>
2021-05-14 07:10:29 +0800

    add chart5

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,6 +4,7 @@ import { Chart1 } from '../components/chart-1';
 import { Chart2 } from '../components/chart-2';
 import { Chart3 } from '../components/chart-3';
 import { Chart4 } from '../components/chart-4';
+import { Chart5 } from '../components/chart-5';
 
 export const Home = () => {
   const currentYear = new Date().getFullYear()
@@ -19,11 +20,13 @@ export const Home = () => {
           <Chart3 />
           <Chart4 />
         </section>
-        <section className="bordered section3"></section>
+        <section className="bordered section3">
+          <Chart5 />
+        </section>
         <section className="bordered section4"></section>
         <section className="bordered section5"></section>
       </main>
-      <footer> &copy; ximuli 2020-{ currentYear }</footer>
+      <footer> &copy; ximuli 2020-{currentYear}</footer>
     </div>
   );
 };
