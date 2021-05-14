a68f0e230682cf57ab7798271e8c64219e9beaed
ximuli <ximuli666@163.com>
2021-05-14 08:03:46 +0800

    add chart6 china geo map

---
+++ src/components/chart-6.tsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useRef } from 'react';
+import * as echarts from 'echarts'
+import { createEchartsOptions } from '../shared/create-echarts-options'
+import { px } from '../shared/px';
+import chinaGeoJson from '../geo/china.json'
+
+export const Chart6 = () => {
+  const divRef = useRef(null)
+  const colors = { '青海省': '#BB31F7', '甘肃省': '#15B8FD', '四川省': '#06E1EE' }
+  useEffect(() => {
+    var myChart = echarts.init(divRef.current);
+    // @ts-ignore
+    echarts.registerMap('CN', chinaGeoJson)
+    var option = createEchartsOptions({
+      xAxis: { show: false },
+      yAxis: { show: false },
+      series: [
+        {
+          type: 'map',
+          mapType: 'CN', // 自定义扩展图表类型
+          data: [
+            // { name: '甘肃省', value: 1 },
+            // {name: '四川省', value: 100},
+          ],
+          // label: { show: false },
+          itemStyle: {
+            areaColor: '#010D3D',
+            // color: colors['甘肃省'],
+            borderColor: '#01A7F7',
+            emphasis: {
+              label: { color: 'white' },
+              areaColor: '#5470C6',
+            },
+          }
+        },
+      ]
+    });
+    myChart.setOption(option);
+  }, [])
+  return (
+    <div className="bordered 籍贯">
+      <h2>全兰州市犯罪人员籍贯分布地</h2>
+      <div className="wrapper">
+        <div ref={divRef} className="chart"></div>
+        {/* <div className="bordered legend">
+          <span className="icon" style={{ background: colors['甘肃省'] }} />甘肃籍
+          <span className="icon" style={{ background: colors['四川省'] }} />四川籍
+          <span className="icon" style={{ background: colors['青海省'] }} />青海籍
+        </div> */}
+        <div className="notes">此地图仅显示了中国的部分区域</div>
+      </div>
+    </div>
+  )
+}
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -5,6 +5,7 @@ import { Chart2 } from '../components/chart-2';
 import { Chart3 } from '../components/chart-3';
 import { Chart4 } from '../components/chart-4';
 import { Chart5 } from '../components/chart-5';
+import { Chart6 } from '../components/chart-6';
 
 export const Home = () => {
   const currentYear = new Date().getFullYear()
@@ -23,7 +24,9 @@ export const Home = () => {
         <section className="bordered section3">
           <Chart5 />
         </section>
-        <section className="bordered section4"></section>
+        <section className="section4">
+          <Chart6 />
+        </section>
         <section className="bordered section5"></section>
       </main>
       <footer> &copy; ximuli 2020-{currentYear}</footer>
